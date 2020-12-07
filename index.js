34513b818ebc37468441c08e7d8b9ae383b85e3a
Tobias Baunbaek <freeall@gmail.com>
2020-12-07 10:08:00 +0100

    Remove boxen. Quit if window is too small

--- index.js
+++ index.js
@@ -51,26 +51,41 @@ async function getBoxedStr () {
   }
 
   const str = [
-    fixedStr('            .,ad88888888baa,', 100),
-    fixedStr('        ,d8P"""        ""9888ba.', 100),
-    fixedStr('     .a8"          ,ad88888888888a', 100),
-    fixedStr('    aP\'          ,88888888888888888a', 100),
-    fixedStr('  ,8"           ,88888888888888888888,', 100),
-    fixedStr(' ,8\'            (888888888( )888888888,', 100),
-    fixedStr(',8\'             `8888888888888888888888', 100),
-    fixedStr(`8)               '888888888888888888888,     uptime    ::  ${info.uptime}`, 100),
-    fixedStr(`8                  "8888888888888888888)     load avg  ::  ${info.load}`, 100),
-    fixedStr(`8                   '888888888888888888)     memory    ::  ${info.memory}`, 100),
-    fixedStr(`8)                    "8888888888888888      ${info.ipsAsStrs[0] || ''}`, 100),
-    fixedStr(`(b                     "88888888888888'      ${info.ipsAsStrs[1] || ''}`, 100),
-    fixedStr(`'8,        (8)          8888888888888)       ${info.ipsAsStrs[2] || ''}`, 100),
-    fixedStr(' "8a                   ,888888888888)', 100),
-    fixedStr('   V8,                 d88888888888"', 100),
-    fixedStr('    `8b,             ,d8888888888P\'', 100),
-    fixedStr('      `V8a,       ,ad8888888888P\'', 100),
-    fixedStr('         ""88888888888888888P"', 100),
-    fixedStr('              """"""""""""', 100)
+    fixedStr('         _..oo8"""Y8b.._', 90),
+    fixedStr('     .88888888o.    "Yb.', 90),
+    fixedStr('   .d888P""Y8888b      "b.', 90),
+    fixedStr('  o88888    88888)       "b', 90),
+    fixedStr(` d888888b..d8888P         'b     uptime    ::  ${info.uptime}`, 90),
+    fixedStr(` 88888888888888"           8     load avg  ::  ${info.load}`, 90),
+    fixedStr(`(88DWB8888888P             8)    ${info.ipsAsStrs[0] || ''}`, 90),
+    fixedStr(` 8888888888P               8     ${info.ipsAsStrs[1] || ''}`, 90),
+    fixedStr(` Y88888888P     ee        .P     ${info.ipsAsStrs[2] || ''}`, 90),
+    fixedStr('  Y888888(     8888      oP', 90),
+    fixedStr('   "Y88888b     ""     oP"', 90),
+    fixedStr('     "Y8888o._     _.oP"', 90),
+    fixedStr('       `""Y888boodP""\'', 90)
   ].join('\n').replace(/ /g, ZERO_WIDTH_SPACE) // Replace all spaces with a similar character. Otherwise chalk-animation won't calculate correctly
+  // const str = [
+  //   fixedStr('            .,ad88888888baa,', 100),
+  //   fixedStr('        ,d8P"""        ""9888ba.', 100),
+  //   fixedStr('     .a8"          ,ad88888888888a', 100),
+  //   fixedStr('    aP\'          ,88888888888888888a', 100),
+  //   fixedStr('  ,8"           ,88888888888888888888,', 100),
+  //   fixedStr(' ,8\'            (888888888( )888888888,', 100),
+  //   fixedStr(',8\'             `8888888888888888888888', 100),
+  //   fixedStr(`8)               '888888888888888888888,     uptime    ::  ${info.uptime}`, 100),
+  //   fixedStr(`8                  "8888888888888888888)     load avg  ::  ${info.load}`, 100),
+  //   fixedStr(`8                   '888888888888888888)     memory    ::  ${info.memory}`, 100),
+  //   fixedStr(`8)                    "8888888888888888      ${info.ipsAsStrs[0] || ''}`, 100),
+  //   fixedStr(`(b                     "88888888888888'      ${info.ipsAsStrs[1] || ''}`, 100),
+  //   fixedStr(`'8,        (8)          8888888888888)       ${info.ipsAsStrs[2] || ''}`, 100),
+  //   fixedStr(' "8a                   ,888888888888)', 100),
+  //   fixedStr('   V8,                 d88888888888"', 100),
+  //   fixedStr('    `8b,             ,d8888888888P\'', 100),
+  //   fixedStr('      `V8a,       ,ad8888888888P\'', 100),
+  //   fixedStr('         ""88888888888888888P"', 100),
+  //   fixedStr('              """"""""""""', 100)
+  // ].join('\n').replace(/ /g, ZERO_WIDTH_SPACE) // Replace all spaces with a similar character. Otherwise chalk-animation won't calculate correctly
   const strRows = str.split('\n').length
   const padding = 1
   const marginTop = Math.max(Math.floor((cliRows - strRows) / 2) - (2 * padding), 0)
